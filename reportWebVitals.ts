774bf607437db5e037c908d62013031e6877e6f2
tronghiant(dan) <tronghiant@users.noreply.github.com>
2022-09-13 13:58:38 +0700

    config `reportWebVitals` in nextJs way

--- reportWebVitals.ts
+++ reportWebVitals.ts
@@ -1,15 +1,7 @@
-import {ReportHandler} from "web-vitals";
+import type {Metric} from "web-vitals";
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import("web-vitals").then(({getCLS, getFID, getFCP, getLCP, getTTFB}) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
-  }
-};
-
-export default reportWebVitals;
+// TODO: Monitoring Web Vitals
+// Ref: https://nextjs.org/learn/seo/monitor/custom-reporting
+export function reportWebVitals(_metric: Metric) {
+  // console.log(metric);
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,6 @@ import {appWithTranslation} from "next-i18next";
 import type {AppProps} from "next/app";
 import {Provider} from "react-redux";
 
-import reportWebVitals from "../../reportWebVitals";
 import Layout from "../components/Layout";
 import {store} from "../ducks/store";
 
@@ -33,5 +32,7 @@ export default appWithTranslation(App);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// or send to an analytics endpoint.
+// Learn more:
+// - https://bit.ly/CRA-vitals
+export {reportWebVitals} from "reportWebVitals";
