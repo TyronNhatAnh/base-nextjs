d1ddf69ec821794edbc2a6b440a93e119952e03e
tronghiant(dan) <tronghiant@users.noreply.github.com>
2022-09-13 18:01:22 +0700

    change default locale to `kr`

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   // https://www.i18next.com/overview/configuration-options#logging
   debug: process.env.NODE_ENV === "development",
   i18n: {
-    defaultLocale: "en",
+    defaultLocale: "kr",
     locales: ["en", "kr"],
   },
   reloadOnPrerender: process.env.NODE_ENV === "development",
